8c05d1e0d2b3f206d17e5c3783fccaa762d962e7
Araba <araba.taylor20@gmail.com>
2018-08-12 20:18:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = 'c0005614919b2895d90ff8afdcb53da653bbf160'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
